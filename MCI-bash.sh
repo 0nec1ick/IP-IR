bfec614db94461af9d08f6033674030213d22cbf
OneClick <125868823+0nec1ick@users.noreply.github.com>
2024-01-19 02:35:25 +0330

    Add files via upload

---
+++ MCI-bash.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+clear
+sudo apt-get install curl -y
+sudo apt-get install ipset -y
+sudo apt-get install iptables-persistent -y
+sudo iptables -F
+sudo ipset destroy
+sudo ipset create MCI hash:net
+sudo ipset create iranipv4 hash:net
+sudo ipset create iranipv6 hash:net family inet6
+for x in $(curl https://raw.githubusercontent.com/0nec1ick/IP-IR/main/MCI.txt); do sudo ipset add MCI $x; done
+for x in $(curl https://raw.githubusercontent.com/0nec1ick/IP-IR/main/iranipv4.txt); do sudo ipset add iranipv4 $x; done
+for x in $(curl https://raw.githubusercontent.com/0nec1ick/IP-IR/main/iranipv6.txt); do sudo ipset add iranipv6 $x; done
+sudo iptables -A INPUT -m set --match-set MCI src -j ACCEPT
+sudo iptables -A OUTPUT -m set --match-set iranipv4 src -j DROP
+sudo ip6tables -A OUTPUT -m set --match-set iranipv6 src -j DROP
+sudo iptables -A INPUT -j DROP
+iptables-save > /etc/iptables/rules.v4
+iptables-save > /etc/iptables/rules.v6
+sudo systemctl enable iptables
+sudo systemctl start iptables
+sudo systemctl is-enabled netfilter-persistent.service
+clear 
+echo "Success , Blocked Iran IP :)"
