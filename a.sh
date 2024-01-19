10f14043f71f7c0ccf9348a14f4b085c1679afc5
OneClick <125868823+0nec1ick@users.noreply.github.com>
2024-01-19 15:53:08 +0330

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo ipset create shervin hash:net
-sudo ipset create MCI hash:net
+sudo ipset create NOMCI.txt hash:net
 sudo ipset create iranipv4 hash:net
 sudo ipset create iranipv6 hash:net family inet6
