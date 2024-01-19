64a746021e186554b872deb3ed1ee7bca006be1c
OneClick <125868823+0nec1ick@users.noreply.github.com>
2024-01-19 15:55:38 +0330

    Update c.sh

--- c.sh
+++ c.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sudo iptables -A INPUT -m set --match-set shervin src -j ACCEPT
-sudo iptables -A INPUT -m set --match-set MCI src -j ACCEPT
-sudo iptables -A INPUT -p tcp --dport 56777 -j DROP
+sudo iptables -A INPUT -m set --match-set NOMCI src -j DROP
 sudo iptables -A OUTPUT -m set --match-set iranipv4 src -j DROP
 sudo ip6tables -A OUTPUT -m set --match-set iranipv6 src -j DROP
