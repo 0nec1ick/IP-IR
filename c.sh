083f41f493a6fd1f2f9ee9d1e0d556d737198877
OneClick <125868823+0nec1ick@users.noreply.github.com>
2024-01-19 05:41:33 +0330

    Update c.sh

--- c.sh
+++ c.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo iptables -A INPUT -m set --match-set shervin src -j ACCEPT
 sudo iptables -A INPUT -m set --match-set MCI src -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 56777 -j DROP
 sudo iptables -A OUTPUT -m set --match-set iranipv4 src -j DROP
 sudo ip6tables -A OUTPUT -m set --match-set iranipv6 src -j DROP
