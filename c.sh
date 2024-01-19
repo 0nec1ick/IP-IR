c58996fa11e4fb69e97300fac378a6a5c8e9b3b5
OneClick <125868823+0nec1ick@users.noreply.github.com>
2024-01-19 04:47:02 +0330

    Create c.sh

---
+++ c.sh
@@ -0,0 +1,4 @@
+sudo iptables -A INPUT -m set --match-set shervin src -j ACCEPT
+sudo iptables -A INPUT -m set --match-set MCI src -j ACCEPT
+sudo iptables -A OUTPUT -m set --match-set iranipv4 src -j DROP
+sudo ip6tables -A OUTPUT -m set --match-set iranipv6 src -j DROP
