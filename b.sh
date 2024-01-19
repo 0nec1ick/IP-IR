0bb0f9e1088bfb6344873900d8f9c01fb1339db8
OneClick <125868823+0nec1ick@users.noreply.github.com>
2024-01-19 15:54:48 +0330

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-for x in $(curl https://raw.githubusercontent.com/0nec1ick/IP-IR/main/shervin.txt); do sudo ipset add shervin $x; done
-for x in $(curl https://raw.githubusercontent.com/0nec1ick/IP-IR/main/MCI.txt); do sudo ipset add MCI $x; done
+for x in $(curl https://raw.githubusercontent.com/0nec1ick/IP-IR/main/NOMCI.txt); do sudo ipset add MCI $x; done
 for x in $(curl https://raw.githubusercontent.com/0nec1ick/IP-IR/main/iranipv4.txt); do sudo ipset add iranipv4 $x; done
 for x in $(curl https://raw.githubusercontent.com/0nec1ick/IP-IR/main/iranipv6.txt); do sudo ipset add iranipv6 $x; done
