f1cab2d3ccdc113d8a168d7307c82110e9f790c4
OneClick <125868823+0nec1ick@users.noreply.github.com>
2024-01-21 20:52:08 +0330

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo apt-get install curl
+sudo apt-get install ipset
 sudo ipset create NOMCI hash:net
 sudo ipset create iranipv4 hash:net
 sudo ipset create iranipv6 hash:net family inet6
