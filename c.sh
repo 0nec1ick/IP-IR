36e1ebe42338643a35d39467f7483d1ce6dd4cfa
OneClick <125868823+0nec1ick@users.noreply.github.com>
2024-01-21 11:02:25 +0330

    Update c.sh

--- c.sh
+++ c.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #sudo iptables -A INPUT -m set --match-set NOMCI src -j DROP
-sudo iptables -A INPUT -p tcp --dport 7345 -m set --match-set NOMCI src -j DROP
+sudo iptables -A INPUT -m set --match-set NOMCI src -j DROP
 sudo iptables -A OUTPUT -m set --match-set iranipv4 src -j DROP
 sudo ip6tables -A OUTPUT -m set --match-set iranipv6 src -j DROP
+sudo systemctl enable iptables
+sudo systemctl start iptables
+sudo systemctl is-enabled netfilter-persistent.service
