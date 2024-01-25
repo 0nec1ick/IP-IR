c71baf1da0596f5480affd071a684943aa21e006
OneClick <125868823+0nec1ick@users.noreply.github.com>
2024-01-25 20:09:46 +0330

    Update firewallold2.sh

--- firewallold2.sh
+++ firewallold2.sh
@@ -2,12 +2,12 @@
 
 sudo apt-get install curl unzip perl xtables-addons-common libtext-csv-xs-perl libmoosex-types-netaddr-ip-perl iptables-persistent -y 
 
-url="https://raw.githubusercontent.com/0nec1ick/IP-IR/main/FullIR.txt"
+url="https://raw.githubusercontent.com/HamedAp/Ssh-User-management/main/i.txt"
 allcount=$(curl -s "$url" | wc -l)
 curl -s "$url"  | while IFS= read -r line; do
 ((++line_number))
-iptables -A OUTPUT -p tcp --dport 80 -d $line -j DROP
-iptables -A OUTPUT -p tcp --dport 443 -d $line -j DROP
+iptables -A OUTPUT -p tcp  --dport 80 -d $line -j DROP
+iptables -A OUTPUT -p tcp  --dport 443 -d $line -j DROP
 clear
 echo "Iran IP Blocking ( List 1 ) : $line_number / $allcount "
 done
