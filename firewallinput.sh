ba270bf8beb9874c711d78cc788be7c23594b0ff
OneClick <125868823+0nec1ick@users.noreply.github.com>
2024-01-25 16:06:00 +0330

    Create firewallinput.sh

---
+++ firewallinput.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+url="https://raw.githubusercontent.com/0nec1ick/IP-IR/main/ipinput.txt"
+allcount=$(curl -s "$url" | wc -l)
+curl -s "$url" | while IFS= read -r line; do
+((++line_number))
+sudo ufw deny out from any to $line
+clear
+echo "Iran IP Blocking Input : $line_number / $allcount "
+done
+ufw enable
