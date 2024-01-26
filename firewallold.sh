a3a076a8d98fa1aee02428e78381663af00b0f4f
OneClick <125868823+0nec1ick@users.noreply.github.com>
2024-01-26 18:25:36 +0330

    Update firewallold.sh

--- firewallold.sh
+++ firewallold.sh
@@ -1,1 +1,24 @@
-1
+#!/bin/bash
+po=$(cat /etc/ssh/sshd_config | grep "^Port")
+pport=$(echo "$po" | sed "s/Port //g")
+if [ -z "$port" ]
+then
+sed -i 's/#Port 22/Port 22/' /etc/ssh/sshd_config
+po=$(cat /etc/ssh/sshd_config | grep "^Port")
+pport=$(echo "$po" | sed "s/Port //g")
+fi
+apt update -y
+apt install ufw  -y
+ufw allow from any to any port $pport proto tcp
+sudo ufw allow $pport
+sudo ufw allow 22/tcp
+sudo ufw allow 80/tcp
+sudo ufw allow 80/udp
+sudo ufw allow 443/tcp
+sudo ufw allow 443/udp
+sudo ufw allow 7300/tcp
+sudo ufw allow 7300/udp
+sudo ufw allow 7575/tcp
+sudo ufw allow 14000/tcp
+sudo ufw allow 56777/tcp
+ufw enable
