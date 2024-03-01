c6d6c2f1030dfbf31af9ad7afdfde81d47b00272
OneClick <125868823+0nec1ick@users.noreply.github.com>
2024-03-01 14:07:31 +0330

    Update firewall-new.sh

--- firewall-new.sh
+++ firewall-new.sh
@@ -2,13 +2,17 @@
 
 timedatectl set-timezone "Asia/Tehran"
 clear
+echo "======================================================================================================="
 echo "Time Set Tehran Successfull"
+echo "======================================================================================================="
 sleep 5
 clear
 sudo apt update -y
 sudo apt install ufw  -y
 sudo apt install curl unzip perl libtext-csv-xs-perl libmoosex-types-netaddr-ip-perl iptables-persistent ipset -y 
+echo "======================================================================================================="
 echo "Install Successfull"
+echo "======================================================================================================="
 sleep 5
 sudo wget -4 -O /root/i.txt https://raw.githubusercontent.com/0nec1ick/IP-IR/main/i.txt &
 wait
@@ -22,19 +26,25 @@ while read line; do ipset add shahaniran $line; done < /root/i.txt
 iptables -A OUTPUT -p tcp --dport 443 -m set --match-set shahaniran dst -j DROP
 iptables -A OUTPUT -p tcp --dport 80 -m set --match-set shahaniran dst -j DROP
 sudo iptables-save | sudo tee /etc/iptables/rules.v4
-sudo iptables-save > /etc/iptables/rules.v4
-sudo ip6tables-save > /etc/iptables/rules.v6
 ipset list
+echo "======================================================================================================="
 echo "IPTable Block Iran IP Successfull"
+echo "======================================================================================================="
 sleep 5
 
 sudo wget -4 -O /etc/ufw/user.rules https://raw.githubusercontent.com/0nec1ick/IP-IR/main/user.rules &
 wait
 
 ufw enable
+echo "======================================================================================================="
 echo "UFW Block Iran IP Successfull"
+echo "======================================================================================================="
 sleep 5
+sudo iptables-save > /etc/iptables/rules.v4
+sudo ip6tables-save > /etc/iptables/rules.v6
 clear
+echo "======================================================================================================="
 echo "Blocked Iran Ip Successfull :)"
+echo "======================================================================================================="
 sleep 5
 reboot
